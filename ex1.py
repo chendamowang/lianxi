740018bb6ab28513504e50679031e87f57a597b1
chenmo <249883376@qq.com>
2018-03-17 14:53:20 +0800

    ex1

---
+++ ex1.py
@@ -0,0 +1,21 @@
+#! user/bin/python
+# -*- coding:utf-8 -*-
+# 有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+
+
+class test(object):
+    def f(self, a):
+        n = 0 
+        for i in a:
+            for j in a:
+                for k in a:
+                    if i != j and i != k and j != k:
+                        print i,j,k
+                        n += 1
+        print n
+        
+
+if __name__ == '__main__':
+    test1 = test()
+    a = [1,2,3,4]
+    test1.f(a)                                         
